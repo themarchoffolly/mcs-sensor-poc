9e3c57610246075e6f8d6fd6a95bcbd810922114
David Russell <themarchoffolly@protonmail.com>
2016-06-24 11:58:13 +0700

    Commented code.

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -11,63 +11,77 @@ import play.Logger;
 import play.Logger.ALogger;
 
 /**
- * This controller contains an action to handle HTTP requests
- * to the application's home page.
+ * Main controller for MCS Sensor POC application.
  */
 public class HomeController extends Controller {
 
     final Logger.ALogger log = Logger.of(this.getClass());
 
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
+    /*
+     * Returns MCS Sensor POC home page.
      */
     public Result index() {
         return ok(index.render());
     }
 
+    /*
+     * Returns R MCS.Sensor::render base64 encoded plot data.
+     */
     public Result plot() {
 
+        /*
+         * Default MCS.sensor::render plot dimensions. These
+         * dimensions can be customized as required by POC.
+         */
         Map data = new HashMap();
         data.put("plotWidth", 750);
         data.put("plotHeight", 320);
 
         OCPUTask oTask = null;
+        String plotData = "";
 
         try {
 
+            /*
+             * Define OCPUTask for MCS.Sensor::render function call.
+             */
             oTask = OCPU.R()
                         .pkg("mcs.poc")
                         .function("render")
                         .input(data)
                         .library();
-        } catch(Exception oex) {
-            log.warn("plot: ex={}", oex);
-        }
 
-        String plotAsString = "";
-
-        if(oTask != null) {
-
+            /*
+             * Execute OCPUTask for MCS.Sensor::render function call.
+             */
             OCPUResult oResult = oTask.execute(OCPU_ENDPOINT);
-            log.info("plot: result isSuccess={}", oResult.success());
-            log.info("plot: result isError={}", oResult.error());
-            log.info("plot: result cause={}", oResult.cause());
+
+            /*
+             * Handle OCPUResult for MCS.Sensor::render function call.
+             */
             if(oResult.output() != null) {
 
+                /*
+                 * Extract base64 encoded plot data from OCPUResult.
+                 */
                 ArrayList plot =
                     (ArrayList) oResult.output().get("render");
-                plotAsString = (String) plot.get(0);
-                log.info("plot: result plotAsString={}", plotAsString);
+                plotData = (String) plot.get(0);
             }
 
         }
 
-        return ok(plotAsString.getBytes()).as("image/png; charset=utf-8; base64,");
+        } catch(Exception oex) {
+            log.warn("plot: ex={}", oex);
+        }
+
+        /*
+         * Returns R MCS.Sensor::render base64 encoded plot data.
+         */
+        return ok(plotData.getBytes()).as(BASE64_ENCODING);
     }
 
     private final String OCPU_ENDPOINT = "http://localhost:4191/ocpu";
+    private final String BASE64_ENCODING = "image/png; charset=utf-8; base64,";
 
 }
